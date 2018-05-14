bf4aaf8c47029f723046ccf40cf08d1ce9b8f621
kevin schuchard <schuchard.kevin@gmail.com>
2018-05-14 17:28:03 -0600

    migrate customService

--- public/app.ts
+++ public/app.ts
@@ -4,9 +4,11 @@ import 'angular-route';
 import homeComponent from './home/home';
 import customersComponent from './customers/customers';
 import customerDetail from './customerDetail/customerDetail';
+import { CustomerService } from './customers/customerService';
 
 angular
   .module('app', ['ngRoute'])
+  .service('customerService', CustomerService)
   .component('home', homeComponent)
   .component('customers', customersComponent)
   .component('customerDetail', customerDetail);
