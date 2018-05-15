5098ccdaeaa40e6fc8b3608d14caea8d5bf4bb27
kevin schuchard <schuchard.kevin@gmail.com>
2018-05-15 09:14:17 -0600

    convert route config to typescript

--- public/app.ts
+++ public/app.ts
@@ -2,16 +2,20 @@ import * as angular from 'angular';
 import 'angular-route';
 
 import hashPrefixConfig from './config.hashprefix';
+import routeProvider from './config.routes';
+
+import CustomerService from './customers/customerService';
+import AddressService from './shared/addressService';
 
 import homeComponent from './home/home';
 import customersComponent from './customers/customers';
 import customerDetailComponent from './customerDetail/customerDetail';
-import CustomerService from './customers/customerService';
-import AddressService from './shared/addressService';
+
 
 angular
   .module('app', ['ngRoute'])
   .config(hashPrefixConfig)
+  .config(routeProvider)
   .service('customerService', CustomerService)
   .service('addressService', AddressService)
   .component('home', homeComponent)
