6c9a4277f90d069877def453706fc1796d5f2db6
kevin schuchard <schuchard.kevin@gmail.com>
2018-05-16 09:30:29 -0600

    minifiy css

--- public/webpack-configs/webpack.prod.js
+++ public/webpack-configs/webpack.prod.js
@@ -1,5 +1,6 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   output: {
@@ -15,6 +16,10 @@ module.exports = {
       filename: '[name].[hash].css',
       chunkFilename: '[id].[hash].css',
     }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessorOptions: { discardComments: { removeAll: true } },
+      canPrint: true
+    })
   ],
   module: {
     rules: [
